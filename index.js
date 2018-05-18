cbf496ca82357ef44c389a98a16939fd133b40be
ImMyst <titouan.galvani@laposte.net>
2018-05-18 10:15:09 +0200

    Connexion to db, design and form done

--- index.js
+++ index.js
@@ -8,32 +8,59 @@ const db = new Sequelize('project1_nodejs', 'user', 'root', {
     dialect: 'mysql'
 });
 
-const Article = db.define('article', {
-    title: { type: Sequelize.TEXT },
-    content: { type: Sequelize.TEXT }
-});
 
 app.set('view engine', 'pug');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('views', 'public/views')
 app.use(express.static('public'));
 
+app.get('/add-review', (req, res) => {
+    res.render('addreview');
+});
+
+const Review = db.define('reviews', {
+        title : { type: Sequelize.STRING },
+        game : { type: Sequelize.STRING},
+        author : { type: Sequelize.STRING },
+        review : { type: Sequelize.STRING },
+        like : { type: Sequelize.INTEGER, defaultValue : 0 },
+        dislike : { type: Sequelize.INTEGER, defaultValue : 0 }
+    });
+
 app.get('/', (req, res) => {
-    Article
-        .findAll()
-        .then((articles) => articles.map(article => article.dataValues))
-        .then((articles) => {
-            res.render('homepage', { articles });
+    Review
+        .sync()
+        .then(()=> {
+          return Review.findAll();
+        })
+        .then((reviews) => {
+            res.render('home', { reviews });
         });
 });
 
 app.post('/', (req, res) => {
-    const { title, content } = req.body;
-    Article
-        .sync()
-        .then(() => Article.create({ title, content }))
-        .then(() => res.redirect('/'));
-});
+    const { title, game, author, review, like, dislike } = req.body;
+    Review
+        .then(() => Review.create({ title, game, author, review, like, dislike }));
+ });
 
+ app.get('/', (req, res) => {
+     Post
+         .findAll({ include: [AddReview] })
+         .then(posts => res.render('home', { posts }));
+ });
+
+ app.post('/api/post', (req, res) => {
+     const { title, game, author, review, like, dislike } = req.body;
+     Post
+         .create({ title, game, author, review, like, dislike })
+         .then(() => res.redirect('/'))
+ });
+
+ app.post('/api/post/:reviewID/addreview', (req, res) => {
+     AddReview
+         .create({ type: 'add', postId: req.params.reviewID })
+         .then(() => res.redirect('/'));
+ });
 
 app.listen(3000);
